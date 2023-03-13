1573c3bdf7e45d29872edddbe0e9edddc71a6aed
laiqi <a21954262@gmail.com>
2023-03-13 18:04:41 +0800

    css revise

--- src/components/ImgList/index.jsx
+++ src/components/ImgList/index.jsx
@@ -5,17 +5,20 @@ import { Row , Col} from "antd";
 
 function ImgList(){
     return(
-        <Row gutter={[24, 24]} className={Styles.section}>
-            {imgs.map(img=>(
-                <Col 
-                    key={img.id}
-                    sm= {{ span: 12}}
-                    lg = {{ span: 6}}
-                >
-                <ImgItem key={img.id} img={img}/> 
-                </Col>
-            ))}
-        </Row>
+        <div className={`${Styles.section} ${Styles.box}`}>
+            <Row gutter={[24, 24]} >
+                {imgs.map(img=>(
+                    <Col 
+                        key={img.id}
+                        sm= {{ span: 12}}
+                        lg = {{ span: 6}}
+                    >
+                    <ImgItem key={img.id} img={img}/> 
+                    </Col>
+                ))}
+            </Row>            
+        </div>
+
     );
 }
 
